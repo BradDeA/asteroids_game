52231c1fa2920af735eea40d89c0320b163b04d5
BradDeA <rcman808@gmail.com>
2025-01-03 14:19:11 -0500

    created asteroid & astroidfield objects, linked them into game loop to create asteroid enemies

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = updatable
+    asteroidfield = AsteroidField()
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
-        player.update(dt)
+        
+        for update in updatable:
+            update.update(dt)
+        
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
         screen.fill('#000000')
-        player.draw(screen)
+
+        for drawn in drawable:
+            drawn.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
